c47d6ded40204b85d36fe0ee378bf3e4071db9c0
Wilson Wu <wilsoncwu@gmail.com>
2017-01-06 15:32:06 -0500

    camera following finished

--- Assets/Scripts/MoveGame.cs
+++ Assets/Scripts/MoveGame.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class MoveGame : MonoBehaviour {
 	private Rigidbody2D rb2d;
+	public float moveSpeed;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		moveSpeed = 0.1f;
 	}
 	
 	void FixedUpdate () {
 
-		/*
         float v = Input.GetAxisRaw("Vertical");
         float h = Input.GetAxisRaw("Horizontal");
-		Debug.Log (h + "" + v);
+		//gameObject.transform.localPosition = new Vector3 (gameObject.transform.localPosition.x + 0.01f, gameObject.transform.localPosition.y + 0.01f, gameObject.transform.localPosition.z);
 
-		rb2d.velocity = new Vector2(h, v);
-		*/
 
-		gameObject.transform.localPosition = new Vector3 (gameObject.transform.localPosition.x + 0.01f, gameObject.transform.localPosition.y + 0.01f, gameObject.transform.localPosition.z);
-
+		rb2d.velocity = new Vector2 (10,10);
 
     }
 }
