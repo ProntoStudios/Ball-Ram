50a2f4d68f00854e70ae5768d785c64f8b29e421
Wilson Wu <wilsoncwu@gmail.com>
2017-01-06 11:24:30 -0500

    physics collisions work

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	private CircleCollider2D projCol;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		projCol = GetComponent<CircleCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter2D(){
+	}
+}
---
+++ Assets/Scripts/ShieldMainScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldMainScript : MonoBehaviour {
+	
+	public float rotateSpeed;
+	private Rigidbody2D rb2d;
+	private BoxCollider2D shieldMainCol; 
+
+	private Vector3 zAxis = new Vector3 (0,0,1);
+
+	// Use this for initialization
+	void Start () {
+		rotateSpeed = 10f;
+		rb2d = GetComponent<Rigidbody2D> ();
+		shieldMainCol = GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.RotateAround (Vector3.zero, zAxis, rotateSpeed);
+		
+
+	}
+}
