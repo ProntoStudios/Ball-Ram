6db44d3680ad038dcdceefd0dfda9f31f2984ba7
Wilson Wu <wilsoncwu@gmail.com>
2017-01-06 13:22:03 -0500

    basic game controller set up

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+	public static GameControl instance;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown (0)){
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex); //restarts game
+		}
+	}
+
+	public void PlayerDied(){
+
+	}
+}
--- Assets/Scripts/ShieldMainScript.cs
+++ Assets/Scripts/ShieldMainScript.cs
@@ -6,15 +6,15 @@ public class ShieldMainScript : MonoBehaviour {
 	
 	public float rotateSpeed;
 	private Rigidbody2D rb2d;
-	private BoxCollider2D shieldMainCol; 
+	private BoxCollider2D shieldCol; 
 
 	private Vector3 zAxis = new Vector3 (0,0,1);
 
 	// Use this for initialization
 	void Start () {
-		rotateSpeed = 10f;
+		rotateSpeed = 2f;
 		rb2d = GetComponent<Rigidbody2D> ();
-		shieldMainCol = GetComponent<BoxCollider2D> ();
+		shieldCol = GetComponent<BoxCollider2D> ();
 	}
 	
 	// Update is called once per frame
