b8cfd2e628d2ea73cbf4fc104ea5591511d45fe5
Brenton Sutherland <brentonsutherland311@gmail.com>
2017-01-06 16:51:36 -0500

    Forgot to add the scripts.

---
+++ Assets/Scripts/SpawnProj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProj : MonoBehaviour {
+    public int maxProjs = 20;
+    private int numProjs = 0;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+	}
+	
+	void CreateObstacle()
+    {
+        if(numProjs < maxProjs)
+        {
+            Instantiate(Resources.Load<GameObject>("Prefabs/ProjSpawn"));
+            numProjs++;
+        }
+    }
+}
