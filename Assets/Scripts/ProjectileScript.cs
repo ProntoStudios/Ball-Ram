f9c99f0c2ef9ba552ea5cff3cd4ef85bb210678f
Wilson Wu <wilsoncwu@gmail.com>
2017-01-06 14:11:00 -0500

    initial setupof merge

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -6,10 +6,15 @@ public class ProjectileScript : MonoBehaviour {
 
 	private Rigidbody2D rb2d;
 	private CircleCollider2D projCol;
+	private int minSpeed, maxSpeed;
+
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 		projCol = GetComponent<CircleCollider2D> ();
+		minSpeed = 1;
+		maxSpeed = 10;
+		rb2d.velocity = new Vector2(Random.Range(minSpeed, maxSpeed), Random.Range(minSpeed, maxSpeed));
 	}
 	
 	// Update is called once per frame
