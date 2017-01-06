7d18577e6046d82cda79014409da17452e4cfe0f
Brenton Sutherland <brentonsutherland311@gmail.com>
2017-01-06 18:24:15 -0500

    GameObject movement and wall collision.

---
+++ Assets/MoveGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(h, v);
+    }
+}
