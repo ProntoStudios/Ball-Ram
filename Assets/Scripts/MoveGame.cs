5686c0cd0fe3413b466ecbf173335929cb8ccc4c
Wilson Wu <wilsoncwu@gmail.com>
2017-01-07 16:57:34 -0500

    added accellerometer, calibrates automatically every time game is restarted. shield also slightly increased in size bc accelerometer movement is way too hard

--- Assets/Scripts/MoveGame.cs
+++ Assets/Scripts/MoveGame.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class MoveGame : MonoBehaviour {
 	private Rigidbody2D rb2d;
 	public float moveSpeed;
+	Matrix4x4 calibrationMatrix;
+
+	void calibrateAccelerometer(){
+		Vector3 wantedDeadZone = Input.acceleration;;
+		Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0f, 0f, -1f), wantedDeadZone);
+		//create identity matrix ... rotate our matrix to match up with down vec
+		Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, rotateQuaternion, new Vector3(1f, 1f, 1f));
+		//get the inverse of the matrix
+		this.calibrationMatrix = matrix.inverse;
+	}
+
+	Vector3 getAccelerometer(Vector3 accelerator){
+		Vector3 accel = this.calibrationMatrix.MultiplyVector(accelerator);
+		return accel;
+	}
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 		moveSpeed = 0.1f;
+		calibrateAccelerometer ();
 	}
 	
 	void FixedUpdate () {
 
-        float v = Input.GetAxisRaw("Vertical");
-        float h = Input.GetAxisRaw("Horizontal");
+        //float v = Input.GetAxisRaw("Vertical");
+		//float h = Input.GetAxisRaw("Horizontal");
+		//Vector3 dir = Vector3.zero;
+		//dir.x = Input.acceleration.y;
+		//dir.z = Input.acceleration.x;
+		Vector3 dir = getAccelerometer(Input.acceleration);
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
 		//gameObject.transform.localPosition = new Vector3 (gameObject.transform.localPosition.x + 0.01f, gameObject.transform.localPosition.y + 0.01f, gameObject.transform.localPosition.z);
 
 
-		rb2d.velocity = new Vector2 (10*h,10*v);
+		rb2d.velocity = new Vector2 (45*dir.x,45*dir.y);
 
     }
 }
