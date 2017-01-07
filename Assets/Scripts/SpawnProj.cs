b0ed268033c6ffb87af40f50fabb34a9b89f5890
Wilson Wu <wilsoncwu@gmail.com>
2017-01-07 00:34:37 -0500

    new hp char by nick

--- Assets/Scripts/SpawnProj.cs
+++ Assets/Scripts/SpawnProj.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnProj : MonoBehaviour {
-    public int maxProjs = 15;
+    public int maxProjs;
     private int numProjs = 0;
 
 	// Use this for initialization
 	void Start () {
+		maxProjs = 1;
         InvokeRepeating("CreateObstacle", 1f, 1.5f);
 	}
 	
@@ -16,7 +17,7 @@ public class SpawnProj : MonoBehaviour {
         if(numProjs < maxProjs)
         {
 			Instantiate(Resources.Load<GameObject>("Prefabs/Proj2"));
-			Instantiate(Resources.Load<GameObject>("Prefabs/Proj0"));
+			Instantiate(Resources.Load<GameObject>("Prefabs/Proj4"));
             numProjs++;
         }
     }
