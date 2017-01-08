47dbab83d8ec68377c643ade1c5cdaec4d494ebb
Brenton Sutherland <brentonsutherland311@gmail.com>
2017-01-08 03:36:57 -0500

    Fixed prefab to spawn entities from individual spawners.

---
+++ Assets/Scripts/UpdateScoreHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScoreHealth : MonoBehaviour
+{
+    public Text text;
+    private PlayerScript player;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        player = GameObject.Find("Player").GetComponents<PlayerScript>()[0];
+        
+        //player = GetComponent<PlayerScript>();
+        //text.text = "Hello There!";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        player = GameObject.Find("Player").GetComponents<PlayerScript>()[0];
+        text.text = "Health: " + player.health;
+	}
+}
