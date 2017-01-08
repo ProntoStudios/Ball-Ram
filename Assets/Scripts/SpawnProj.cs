e69226a8cd0bd81f276132fd95b5f2a6b7f13ed9
Brenton Sutherland <brentonsutherland311@gmail.com>
2017-01-08 03:29:15 -0500

    Added basic spawner.

--- Assets/Scripts/SpawnProj.cs
+++ Assets/Scripts/SpawnProj.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SpawnProj : MonoBehaviour {
     public int maxProjs;
+    //public 
     private int numProjs = 0;
+    public GameObject prefab = Resources.Load<GameObject>("Prefabs/Spawner");
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		maxProjs = 100;
         InvokeRepeating("CreateObstacle", 1.5f, 1.5f);
 	}
@@ -16,7 +18,10 @@ public class SpawnProj : MonoBehaviour {
     {
         if(numProjs < maxProjs)
 		{
-			Instantiate(Resources.Load<GameObject>("Prefabs/Proj" + Random.Range(0,5)));
+            GameObject proj = Resources.Load<GameObject>("Prefabs/Proj" + Random.Range(0, 5));
+            Vector3 position = prefab.transform.position;
+            Debug.Log(position.x + ", " + position.y + ", " + position.z);
+            Instantiate(proj, prefab.transform.position, Quaternion.Euler(0, 0, 0));
             numProjs++;
         }
     }
