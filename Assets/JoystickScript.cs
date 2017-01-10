e2bd1f61f741100858c4ebdede4a8be5f6951ecc
Wilson Wu <wilsoncwu@gmail.com>
2017-01-09 19:32:24 -0500

    joystick:

---
+++ Assets/JoystickScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickScript : MonoBehaviour {
+	Vector2 curTouchPos;
+	Vector2 relTouchPos;
+	Vector2 relTouchPosNorm;
+	static Vector2 centerPos = new Vector2 (Screen.height/2f, Screen.width/2f);
+	bool isTouching = false;
+	// Use this for initialization
+	void Start () {
+		curTouchPos = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int numTouch = Input.touchCount;
+		if (numTouch == 0) {
+			isTouching = false;
+			gameObject.transform.position = Vector2.zero;
+
+		}
+		else if (numTouch > 0) {
+			curTouchPos = Input.GetTouch (0).position;
+			relTouchPos.Set (curTouchPos.x - centerPos.x, curTouchPos.y - centerPos.y);
+			relTouchPosNorm = relTouchPos.normalized;
+
+			if (isTouching) {
+
+			} else {
+				isTouching = true;
+				//TODO: SET LOCATION OF JOYSTICK
+			}
+			Debug.Log (centerPos);
+			Debug.Log (curTouchPos);
+			Debug.Log (relTouchPos);
+		}
+
+	}
+}
---
+++ Assets/Scripts/powerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerUp : MonoBehaviour {
+
+    private int powerUpNumber;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	public int PowerUpNumber
+    {
+        get { return powerUpNumber; }
+        set { powerUpNumber = value; }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+
+            collision.gameObject.GetComponent<PlayerScript>().PowerUp = powerUpNumber;
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
