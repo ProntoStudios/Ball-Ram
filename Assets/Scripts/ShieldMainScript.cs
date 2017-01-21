da5d7e3eb6b2205165450fe1cf01afb46bb72f54
Wilson Wu <wilsoncwu@gmail.com>
2017-01-21 06:42:44 -0500

    smoother

--- Assets/Scripts/ShieldMainScript.cs
+++ Assets/Scripts/ShieldMainScript.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShieldMainScript : MonoBehaviour {
-	private Rigidbody2D rb2d;
-	private BoxCollider2D shieldCol; 
 
 	private Vector3 zAxis = new Vector3 (0,0,1);
 
 	// Use this for initialization
 	void Start () {
-		//rotateSpeed = 2f;
-		rb2d = GetComponent<Rigidbody2D> ();
-		shieldCol = GetComponent<BoxCollider2D> ();
-		GameControl.instance.rotateSpeed = 3f;
+		GameControl.instance.rotateSpeed = 2f;
 	}
 	
 	// Update is called once per frame
