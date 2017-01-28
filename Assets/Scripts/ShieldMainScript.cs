d6a570a5acd61c6d08cde42454d7f6a59a4478f2
Wilson Wu <wilsoncwu@gmail.com>
2017-01-27 21:20:16 -0500

    weak shields option

--- Assets/Scripts/ShieldMainScript.cs
+++ Assets/Scripts/ShieldMainScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShieldMainScript : MonoBehaviour {
 
 	private Vector3 zAxis = new Vector3 (0,0,1);
+	public bool weakShields = false;
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +24,28 @@ public class ShieldMainScript : MonoBehaviour {
 		//gameObject.transform.localScale = (Vector3)gameObject.transform.localScale + new Vector3 (0.1f,0,0);
 
 	}
+	void OnCollisionEnter2D(Collision2D node){
+		if (weakShields) {
+			if (node.gameObject.tag == "Projectile") {
+				StartCoroutine (disableFor (2f));
+			}
+		}
+	}
 	public Vector3 toPlayerNorm (){
 		return (PlayerScript.instance.transform.position - gameObject.transform.position).normalized;
+	}
+	public IEnumerator disableFor(float seconds){
+		float currScale = gameObject.transform.localScale.x;
+		while (gameObject.transform.localScale.x > 0.05f) {
+			gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x - 0.07f, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+			yield return new WaitForSeconds (0.015f);
+		}
+		gameObject.transform.localScale = new Vector3(0, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+		yield return new WaitForSeconds (seconds);
+		while (gameObject.transform.localScale.x < currScale) {
+			gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x + 0.07f, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+			yield return new WaitForSeconds (0.015f);
+		}
+		gameObject.transform.localScale = new Vector3(currScale, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
 	}
 }
