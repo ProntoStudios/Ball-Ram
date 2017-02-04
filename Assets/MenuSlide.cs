c508e657dbf539c7e55ad5c7577c21f32287d70e
Colin Daly <dalyco884@gmail.com>
2017-02-04 01:34:19 -0500

    add assetS

---
+++ Assets/MenuSlide.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSlide : MonoBehaviour {
+
+	public float slidespeed = 2.0f;
+	private Vector3 origpos;
+	public bool fromLeft = true;
+	public float shiftdist = 20;
+
+
+	// Use this for initialization
+	void Start () {
+		origpos = gameObject.transform.localPosition;
+		Vector3 shifted = origpos;
+		if (fromLeft) {
+			shifted.x -= shiftdist;
+		} else {
+			shifted.x += shiftdist;
+		}
+		gameObject.transform.localPosition = shifted;
+		StartCoroutine (slideback ());
+	}
+
+	private IEnumerator slideback(){
+		if (fromLeft) {
+			while (gameObject.transform.localPosition.x <= origpos.x) {
+				gameObject.transform.localPosition = new Vector3 (gameObject.transform.localPosition.x + slidespeed, origpos.y, origpos.z);
+				yield return new WaitForSeconds (0.01f);
+			}
+		} 
+		else {
+			while (gameObject.transform.localPosition.x >= origpos.x) {
+				gameObject.transform.localPosition = new Vector3 (gameObject.transform.localPosition.x - slidespeed, origpos.y, origpos.z);
+				yield return new WaitForSeconds (0.01f);
+			}
+		}
+
+	}
+
+}
---
+++ Assets/MenuSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSpawner : MonoBehaviour {
+
+	GameObject proj0;
+	GameObject proj1;
+	public int spawnrate = 200;
+	private int counter = 0;
+	private bool foundPrefabs = false;
+
+	// Use this for initialization
+	void Start () {
+		proj0 = Resources.Load<GameObject> ("Prefabs/proj0");
+		proj1 = Resources.Load<GameObject> ("Prefabs/proj1");
+		foundPrefabs = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		counter++;
+
+		if (counter >= spawnrate&&foundPrefabs) {
+			Vector3 pos = gameObject.transform.localPosition;
+			Instantiate (proj0, pos, Quaternion.identity);
+			Instantiate (proj1, pos, Quaternion.identity);
+			counter = 0;
+		}
+	}
+}
